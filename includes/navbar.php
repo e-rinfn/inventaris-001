c36b0d944cc635fe34e9b2393864573001dc849a
e-rinfn <erinfajrinnugraha@gmail.com>
2026-01-02 09:15:06 +0700

    perbaikan tampilan navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -9,6 +9,6 @@
         <span>Hai, <?= htmlspecialchars($_SESSION['nama_lengkap'] ?? $_SESSION['username']) ?></span>
         <div class="avatar"><?= strtoupper(substr($_SESSION['username'], 0, 1)) ?></div>
         <a href="<?= $base_url ?>/pages/auth/profile.php">Profil</a> |
-        <a href="<?= $base_url ?>/pages/auth/logout.php">Logout</a>
+        <a style="background-color: red;" href="<?= $base_url ?>/pages/auth/logout.php">Logout</a>
     </div>
 </div>
