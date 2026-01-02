45fdf6c4387f863852a695e4d90bd7b529571092
e-rinfn <erinfajrinnugraha@gmail.com>
2026-01-02 09:25:32 +0700

    perbaikan favicon

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Sistem Inventaris</title>
-  <link rel="icon" type="image/x-icon" href="../../assets/img/favicon/favicon.ico">
+  <link rel="icon" type="image/x-icon" href="../../assets/img/favicon/Logo.png">
   <link rel="stylesheet" href="../../assets/css/simple.css?v=<?= time() ?>">
 </head>
 
